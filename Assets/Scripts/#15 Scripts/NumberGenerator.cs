101d1209625d9def208d1b2919962a56188311e7
JasurD <jasur.shuxratovich@gmail.com>
2020-08-04 13:31:07 +0500

    Generator for Arbuz :3

--- Assets/Scripts/#15 Scripts/NumberGenerator.cs
+++ Assets/Scripts/#15 Scripts/NumberGenerator.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 
@@ -9,21 +10,66 @@ public class NumberGenerator : MonoBehaviour
     [SerializeField] private GameObject gate;
     [SerializeField] private GameObject[] clouds;
     
+    private List<int> uniqueNumbers;
+    private bool listCreated = false;
+
+    private int[] _options = new int[9];
+    private int[] _terms = new int[8];
+    
     
     [Header("Parameters")]
     [SerializeField] private int numberToGenerate;
     
     public void Start()
     {
+        uniqueNumbers = new List<int>();
         GenerateNumber(numberToGenerate);
         gate.GetComponentInChildren<TextMeshProUGUI>().text = numberToGenerate.ToString();
     }
 
     private void GenerateNumber(int number)
     {
+        CreateRandomList(1, numberToGenerate);
+        for (int i = 0; i < 8; i += 2)
+        {
+            _terms[i] = GetUniqueRandom(1, numberToGenerate);
+            _terms[i+1] = numberToGenerate - _terms[i];
+        }
+
+        CreateRandomList(1, 9);
+        for (int i = 0; i < 9; i++)
+        {
+            _options[GetUniqueRandom(0, 9)] = _terms[i];
+        }
+
         foreach (var cloud in clouds)
         {
             cloud.GetComponentInChildren<TextMeshProUGUI>().text = Random.Range(1, number).ToString();
         }
     }
+
+    public void CreateRandomList(int min, int max)
+    {
+        uniqueNumbers.Clear();
+
+        for(int i = min; i < max; i++)
+        {
+            uniqueNumbers.Add(i);
+        }
+    }
+    public int GetUniqueRandom(int min, int max)
+    {
+        int randNum = 0;
+
+        if (uniqueNumbers.Count == 0)
+        {
+            CreateRandomList(min, max);
+        }
+
+        randNum = uniqueNumbers[Random.Range(0,uniqueNumbers.Count)];
+        uniqueNumbers.Remove(randNum);
+        
+        return randNum;
+    } 
+
 }
